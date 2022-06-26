28b907ae4de1bba3411b67a215c8faf3445a7c31
AryaaSk <aryaa_saravana@outlook.com>
2022-06-26 18:52:52 +0100

    Left the testing image in by default by accident

--- dist/main.js
+++ dist/main.js
@@ -284,7 +284,7 @@ const Main = () => {
     const [file, print] = [document.getElementById("addImage"), document.getElementById("printButton")];
     const [canvas, transformOverlay, rotateButton, bringForwardButton, deleteButton, duplicateButton] = [paper.getContext('2d'), document.getElementById("transformOverlay"), document.getElementById("rotateButton"), document.getElementById("bringForward"), document.getElementById("delete"), document.getElementById("duplicate")];
     const [topLeftResize, topRightResize, bottomLeftResize, bottomRightResize] = [document.getElementById("topLeftResize"), document.getElementById("topRightResize"), document.getElementById("bottomLeftResize"), document.getElementById("bottomRightResize")];
-    IMAGES.push(NewImageObject("performanceTest.png", 1496, 1200)); //for testing
+    //IMAGES.push(NewImageObject("performanceTest.png", 1496, 1200)); //for testing
     body.style.setProperty("--resizeCounterRadius", `${TRANSFORM_OVERLAY_RESIZE_RADIUS}px`);
     InitHTML(taskbar);
     FitToScreen();
